161c339337a419575b46c0a4fc331f44cd43b289
Mauro Filho <mauro.filho@gmail.com>
2018-06-20 07:00:48 -0300

    feat(Ping): add ping endpoint

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	router := configureRouter()
+	router.Run()
+}
+
+func configureRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return router
 }
