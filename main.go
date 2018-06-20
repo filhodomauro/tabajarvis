49a21a0af2acc5a80b0ad751bd5a661cd5e084ee
Mauro Filho <mauro.filho@gmail.com>
2018-06-20 07:25:31 -0300

    feat(Subscribe): add subscribe endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +15,20 @@ func configureRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
+	})
+
+	router.GET("/webhook", func(c *gin.Context) {
+		verifyToken := os.Getenv("VERIFY_TOKEN")
+		challenge := c.Params.ByName("hub.challenge")
+		mode := c.Params.ByName("hub.mode")
+		facebookVerifyToken := c.Params.ByName("hub.verify_token")
+
+		if mode == "subscribe" && facebookVerifyToken == verifyToken {
+			c.String(200, challenge)
+		} else {
+			c.String(403, "Unnautorized")
+		}
+
 	})
 	return router
 }
